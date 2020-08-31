bf695cb1f3ba3080b95da83a16bf388a0fb27c55
AriTedeschi <arijun2010@hotmail.com>
2020-08-31 20:35:37 -0300

    #1234 Resource cliente/(id)

---
+++ src/main/java/com/example/cursorestful_springboot/Controllers/Cliente.java
@@ -0,0 +1,10 @@
+package com.example.cursorestful_springboot.Controllers;
+
+public class Cliente {
+    
+    public  int     id;
+    public  String  nome;
+    public  String  endereco;
+    public  double  saldo;
+
+}
--- src/main/java/com/example/cursorestful_springboot/Controllers/ClienteController.java
+++ src/main/java/com/example/cursorestful_springboot/Controllers/ClienteController.java
@@ -1,19 +1,55 @@
 package com.example.cursorestful_springboot.Controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClienteController{
+
+    public List<Cliente> clientes;
+
+    @PostConstruct
+    public void init(){
+        Cliente c1  = new Cliente();
+
+        c1.id       = 1;
+        c1.nome     = "José";
+        c1.endereco = "Rua x, 89";
+        c1.saldo    = 202.0;
+
+        Cliente c2  = new Cliente();
+        
+        c2.id       = 2;
+        c2.nome     = "Maria";
+        c2.endereco = "Rua y, 79";
+        c2.saldo    = 444.0;
+
+        clientes = Arrays.asList(c1,c2);
+    }
+
     
     @GetMapping("/clientes")
-    public String getClientes(){
-        return "Retorno de todos os clientes";
+    public List<Cliente> getClientes(){
+        return clientes;
     }
     
-    @GetMapping("/clientes/{codigo}")
-    public String getClienteByCodigo(@PathVariable int codigo){
-        return "Retorna cliente de código="+codigo;
+    @GetMapping("/clientes/{id}")
+    public Cliente getClienteByCodigo(@PathVariable int id){
+        Cliente cliente = null;
+
+        for (Cliente aux : clientes) {
+            if(aux.id == id){
+                cliente = aux;
+                break;
+            }
+        }
+        
+        return cliente;
     }
 }
